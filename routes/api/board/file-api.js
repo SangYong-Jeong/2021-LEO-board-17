7b0370a284838234774e19699807dc2bf894d7c5
SangYong-Jeong <mmaf@naver.com>
2021-09-16 17:40:45 +0900

    api 완료 delete 최종수정만 하고 댓글로

--- routes/api/board/file-api.js
+++ routes/api/board/file-api.js
@@ -1,11 +1,19 @@
 const path = require('path')
 const express = require('express')
 const router = express.Router()
+const {moveFile} = require('../../../modules/util')
 const { pool } = require('../../../modules/mysql-init')
 
 router.delete('/:id' , async (req, res, next) => {
+	let sql, values
 	try {
-		res.status(200).send('hi')
+		sql = " UPDATE files SET status = '0' WHERE fid = " + req.params.id
+		await pool.execute(sql)
+
+		sql = " SELECT saveName FROM files WHERE fid = " + req.params.id
+		const [[rs]] = await pool.execute(sql)
+		moveFile(rs.saveName)
+		res.status(200).json({code: 200, result: 'success'})
 	}
 	catch(err) {
 		res.status(500).json(err)
